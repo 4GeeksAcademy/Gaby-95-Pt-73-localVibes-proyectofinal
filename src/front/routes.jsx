41f8383db478325f0781c033ed2fac192695a641
edgm2003 <54037279+edgm2003@users.noreply.github.com>
2026-08-20 02:35:52 +0000

    Events And Details 1

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -4,17 +4,24 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { Profile } from "./pages/Profile";
+import { Events } from "./pages/Events"; // 1. IMPORTAMOS LA PÁGINA DE EVENTOS
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>}>
-            {/* 'index' define la página inicial por defecto al entrar a la raíz / */}
+        <Route path="/" element={<Layout />} errorElement={<h1 className="text-center mt-5">Not found!</h1>}>
+            
+            {/* Ruta inicial por defecto */}
             <Route index element={<Signup />} />
             
+            {/* Rutas de Autenticación */}
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
+            
+            {/* Rutas Principales */}
             <Route path="/home" element={<Home />} />
+            <Route path="/events" element={<Events />} /> {/* 2. REGISTRAMOS LA RUTA /events */}
+            
         </Route>
     )
 );
